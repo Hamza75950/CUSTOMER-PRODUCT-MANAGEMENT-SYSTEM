daca5ceb35428787ff89a0b4dded24b1a66dba60
Fahad9603 <fahadtasleem96@gmail.com>
2023-10-11 10:46:33 +0500

    Add project files.

---
+++ Final_Project/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Final_Project.Models;
+
+namespace Final_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        //khttps://labpys.com/how-to-create-login-page-using-asp-net-core-mvc-c-with-database/
+
+        //private readonly Login _loginUser;
+
+        //public LoginController(Login loguser)
+        //{
+        //    _loginUser = loguser;
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string passcode)
+        {
+            var issuccess = username;  //_loginUser.AuthenticateUser(username, passcode);
+
+
+            if (issuccess.Length != null)
+            {
+                ViewBag.username = string.Format("Successfully logged-in", username);
+
+                TempData["username"] = "Ahmed";
+                return RedirectToAction("Dashboard","Home");
+            }
+            else
+            {
+                ViewBag.username = string.Format("Login Failed ", username);
+                return View();
+            }
+        }
+    }
+}
---
+++ Final_Project/Models/User_Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final_Project.Models
+{
+    public class User_Login
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username")]
+        [Display(Name = "Please Enter Username")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        [Display(Name = "Please Enter Password")]
+        public string passcode { get; set; }
+        public int isActive { get; set; }
+    }
+}
